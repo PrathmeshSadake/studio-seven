c9884401d497e428dcec9dc2d669a0d4f05f1569
Prathmesh Sadake <prathmeshsadake@gmail.com>
2022-01-07 20:35:54 +0530

    created auth route

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const usersRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
 
 const app = express();
 const db = process.env.MONGO_URI;
@@ -14,6 +15,7 @@ mongoose
 app.use(express.json());
 
 app.use("/api/users", usersRoute);
+app.use("/api/auth", authRoute);
 
 app.get("/", (req, res) => console.log(`Server Running`));
 
